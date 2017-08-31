2a40f8fc5a41bcf7df4392fb4cc061ce881bc659
Wei Li <sirius.liwei@huawei.com>
2017-08-31 14:20:08 -0400

    sdm710: remove 'reboot edl' interface for security.
    BUG: 63100473
    
    Change-Id: If6cb5ce50524d34b30f09a22eeb76d2cd30ae834
    Signed-off-by: Albert I <kras@raphielgang.org>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -165,6 +165,7 @@ static bool get_dload_mode(void)
 	return dload_mode_enabled;
 }
 
+#if 0
 static void enable_emergency_dload_mode(void)
 {
 	int ret;
@@ -191,6 +192,7 @@ static void enable_emergency_dload_mode(void)
 	if (ret)
 		pr_err("Failed to set secure EDLOAD mode: %d\n", ret);
 }
+#endif
 
 static int dload_set(const char *val, const struct kernel_param *kp)
 {
@@ -219,10 +221,12 @@ static void set_dload_mode(int on)
 	return;
 }
 
+#if 0
 static void enable_emergency_dload_mode(void)
 {
 	pr_err("dload mode is not enabled on target\n");
 }
+#endif
 
 static bool get_dload_mode(void)
 {
@@ -363,8 +367,10 @@ static void msm_restart_prepare(const char *cmd)
 				__raw_writel(0x6f656d00 | (code & 0xff),
 					     restart_reason);
 			}
+#if 0
 		} else if (!strncmp(cmd, "edl", 3)) {
 			enable_emergency_dload_mode();
+#endif
 		} else {
 			__raw_writel(0x77665501, restart_reason);
 		}
