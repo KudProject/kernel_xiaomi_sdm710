f03f776e438a58f4f9f8858abac5a90e6a13a173
Albert I <kras@raphielgang.org>
2019-08-10 14:54:25 +0800

    qcacld-3.0: Mark sap_get_csa_reason_str as __maybe_unused
    * Only used in QDF_TRACE, which is already no-op here.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- drivers/staging/qcacld-3.0/core/sap/src/sap_module.c
+++ drivers/staging/qcacld-3.0/core/sap/src/sap_module.c
@@ -1257,7 +1257,7 @@ wlansap_update_csa_channel_params(struct sap_context *sap_context,
  *
  * Return: string reason
  */
-static char *sap_get_csa_reason_str(enum sap_csa_reason_code reason)
+static char __maybe_unused *sap_get_csa_reason_str(enum sap_csa_reason_code reason)
 {
 	switch (reason) {
 	case CSA_REASON_UNKNOWN:
