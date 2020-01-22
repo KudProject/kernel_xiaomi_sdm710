6a45990181d82cca89792edbeda77fc0605f3766
Dhanalakshmi Siddani <dsiddani@codeaurora.org>
2020-01-22 12:13:24 +0530

    dsp: Correct indentation
    Correct indentation to fix compilation failure.
    
    Change-Id: I1dd727fbc562a39feef0dd3e177903b9a77367df
    Signed-off-by: Dhanalakshmi Siddani <dsiddani@codeaurora.org>

--- dsp/q6asm.c
+++ dsp/q6asm.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2020, The Linux Foundation. All rights reserved.
  * Author: Brian Swetland <swetland@google.com>
  *
  * This software is licensed under the terms of the GNU General Public
@@ -1783,7 +1783,7 @@ static int32_t q6asm_srvc_callback(struct apr_client_data *data, void *priv)
 		if ((session_id > 0 && session_id <= SESSION_MAX))
 			spin_unlock_irqrestore(
 				&(session[session_id].session_lock), flags);
-			return 0;
+		return 0;
 	}
 	port = &ac->port[dir];
 
