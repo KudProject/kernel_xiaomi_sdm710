MotorolaMobilityLLC/kernel-msm

Remove Per File Key based hardware crypto framework
Remove the Per File Key logic based inline crypto support
for file encryption framework.

Change-Id: I90071562ba5c41b9db470363edac35c9fe5e4efa
Signed-off-by: Neeraj Soni <neersoni@codeaurora.org>

fs/f2fs/f2fs.h
@@ -4035,8 +4035,7 @@ static inline bool f2fs_force_buffered_io(struct inode *inode,
 	struct f2fs_sb_info *sbi = F2FS_I_SB(inode);
 	int rw = iov_iter_rw(iter);
 
-	if (f2fs_encrypted_file(inode) &&
-	    !fscrypt_using_hardware_encryption(inode))
+	if (f2fs_encrypted_file(inode))
 		return true;
 	if (f2fs_is_multi_device(sbi))
 		return true;
@@ -4059,16 +4058,6 @@ static inline bool f2fs_force_buffered_io(struct inode *inode,
 		return true;
 
 	return false;
-}
-
-static inline bool f2fs_may_encrypt_bio(struct inode *inode,
-		struct f2fs_io_info *fio)
-{
-	if (fio && (fio->type != DATA || fio->encrypted_page))
-		return false;
-
-	return (f2fs_encrypted_file(inode) &&
-			fscrypt_using_hardware_encryption(inode));
 }
 
 #ifdef CONFIG_F2FS_FAULT_INJECTION

KudProject/kernel_xiaomi_sdm710

[kras: backported and adjusted for msm-4.9, defconfig skipped]
Signed-off-by: Albert I <kras@raphielgang.org>

fs/f2fs/f2fs.h
@@ -3719,8 +3719,7 @@ static inline bool f2fs_force_buffered_io(struct inode *inode,
 	struct f2fs_sb_info *sbi = F2FS_I_SB(inode);
 	int rw = iov_iter_rw(iter);
 
-	if (f2fs_post_read_required(inode) &&
-		!fscrypt_using_hardware_encryption(inode))
+	if (f2fs_post_read_required(inode))
 		return true;
 	if (f2fs_is_multi_device(sbi))
 		return true;
@@ -3741,16 +3740,6 @@ static inline bool f2fs_force_buffered_io(struct inode *inode,
 		return true;
 
 	return false;
-}
-
-static inline bool f2fs_may_encrypt_bio(struct inode *inode,
-		struct f2fs_io_info *fio)
-{
-	if (fio && (fio->type != DATA || fio->encrypted_page))
-		return false;
-
-	return (f2fs_encrypted_file(inode) &&
-			fscrypt_using_hardware_encryption(inode));
 }
 
 #ifdef CONFIG_F2FS_FAULT_INJECTION